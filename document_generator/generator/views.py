2317e1890e51b0f9364c37fe26133dd071b862a0
Ruslan Zakirov <ruslanzak5@gmail.com>
2017-07-15 19:06:44 +0300

    design fix

--- document_generator/generator/views.py
+++ document_generator/generator/views.py
@@ -1,7 +1,8 @@
 import random
 import string
 
-from django.shortcuts import render_to_response, render,redirect
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render_to_response, render, redirect
 from .models import *
 from .forms import *
 from django.contrib.auth import get_user_model
@@ -19,6 +20,7 @@ def edit_individual(request):
     pass
 
 
+@user_passes_test(lambda u: Group.objects.get(name='Deanery') in u.groups.all())
 def new_practice(request):
     title = "Добавление практики"
     if request.POST:
@@ -32,6 +34,7 @@ def new_practice(request):
 
 
 
+@user_passes_test(lambda u: Group.objects.get(name='Deanery') in u.groups.all())
 def edit_practice(request, id):
     title = "Редактирование практики"
     practice = Practice.objects.get(pk=id)
@@ -47,6 +50,7 @@ def edit_practice(request, id):
         return render(request, 'deanery/practice.html', locals())
 
 
+@user_passes_test(lambda u: Group.objects.get(name='Deanery') in u.groups.all())
 def new_student(request):
     title = "Добавление студента"
     if request.POST:
@@ -55,7 +59,7 @@ def new_student(request):
             student = form.save(commit=False)
             login = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
             password = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
-            user = get_user_model().objects.create_user(username=login,password=password)
+            user = get_user_model().objects.create_user(username=login, password=password)
             Group.objects.get(name='Students').user_set.add(user)
             student.login = login
             student.password = password
@@ -80,6 +84,7 @@ def student_pass(request, id):
     pass
 
 
+@user_passes_test(lambda u: Group.objects.get(name='Deanery') in u.groups.all())
 def edit_student(request, id):
     title = "Редактирование студента"
     student = Student.objects.get(pk=id)
@@ -93,6 +98,7 @@ def edit_student(request, id):
         return render(request, 'deanery/student.html', {'form': form}, locals())
 
 
+@user_passes_test(lambda u: Group.objects.get(name='Deanery') in u.groups.all())
 def practices(request):
     title = "Практики"
     practices_list = Practice.objects.all()
@@ -103,6 +109,7 @@ def edit_diary(request):
     pass
 
 
+@user_passes_test(lambda u: Group.objects.get(name='Deanery') in u.groups.all())
 def students(request):
     title = "Студенты"
     students_list = Student.objects.all()
