4cbcf52114ba72d8b546eab963fc7cd8714fdbd1
Ruslan Zakirov <ruslanzak5@gmail.com>
2017-07-20 20:39:01 +0300

    passView edit

--- document_generator/generator/forms.py
+++ document_generator/generator/forms.py
@@ -35,3 +35,10 @@ class IndividualTaskForm(ModelForm):
     class Meta:
         model = IndividualTask
         exclude = ['student']
+
+
+class PassForm(Form):
+    necessary_works = CharField(max_length=80)  # в каких видах работ нуждается
+    report = TextField(max_length=1429)  # big field 1
+    review = TextField(max_length=1034)  # big field 2
+    mark = CharField(max_length=20)  # оценка
